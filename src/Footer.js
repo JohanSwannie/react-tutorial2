2f424afbb86760b83b648f17c169a32ed2d6291a
jswan <jswannieman@gmail.com>
2023-03-11 19:25:52 +1100

    Update Footer Javascript Component

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-const Footer = () => {
+const Footer = ({ length }) => {
   const today = new Date();
   return (
     <footer>
+      <p>
+        {length} List {length === 1 ? "Item" : "Items"}
+      </p>
       <h5>Copyright &copy; {today.getFullYear()}</h5>
     </footer>
   );
