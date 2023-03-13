a529bb457099a92c65ca357b0320ffffcb6a334d
jswan <jswannieman@gmail.com>
2023-03-13 12:40:50 +1100

    Commit MainContent JS Component

---
+++ src/MainContent.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Items from "./Items";
+
+const MainContent = ({ items, handleCheck, handleDelete }) => {
+  return (
+    <main>
+      {items.length ? (
+        <Items
+          items={items}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />
+      ) : (
+        <p style={{ marginTop: "2rem" }}>Your list is empty.</p>
+      )}
+    </main>
+  );
+};
+
+export default MainContent;
